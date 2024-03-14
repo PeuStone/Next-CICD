2540064fc3d470e9f8c3c5192e6df377e8967a2f
PeuStone <pedrohmotaaa@gmail.com>
2024-03-14 11:22:05 -0300

    update

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -3,8 +3,11 @@ import { execSync } from "child_process";
 console.log("[DEPLOY _PREVIEW]: START");
 const command = "yarn deploy:staging";
 const output = execSync(command, { encoding: "utf-8" });
-console.log(output);
+const outputLines = output.split("\n");
+const DEPLOY_URL = outputLines[outputLines.length - 1];
 console.log("[DEPLOY _PREVIEW]: END");
+
+console.log(`Você pode ver o preview do deploy em: ${DEPLOY_URL}`);
 
 // console.log("[GITHUB_COMMENT]: START");
 // console.log("[GITHUB_COMMENT]: END");
