35e9d7078139cfdedf373b64f11360cd193a695a
PeuStone <pedrohmotaaa@gmail.com>
2024-03-15 09:31:44 -0300

    Adicionando o GitHub Comments ao Pull Request Preview

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -9,5 +9,42 @@ console.log("[DEPLOY _PREVIEW]: END");
 
 console.log(`Você pode ver o preview do deploy em: ${DEPLOY_URL}`);
 
-// console.log("[GITHUB_COMMENT]: START");
-// console.log("[GITHUB_COMMENT]: END");
+// ===================================================================
+// ===================================================================
+
+console.log("[GITHUB_COMMENT]: START");
+const { GITHUB_REPOSITORY, GITHUB_PR_NUMBER, GITHUB_TOKEN } = process.env;
+const GH_COMMENT = `
+  - Deploy URL: [${vercelUrl}](${vercelUrl})
+`;
+
+const defaultHeaders = {};
+defaultHeaders["authorization"] = `token ${GITHUB_TOKEN}`;
+defaultHeaders["accept"] =
+  "application/vnd.github.v3+json; application/vnd.github.antiope-preview+json";
+defaultHeaders["content-type"] = "application/json";
+
+console.log("GITHUB_REPOSITORY", GITHUB_REPOSITORY);
+console.log("GITHUB_PR_NUMBER", GITHUB_PR_NUMBER);
+
+fetch(
+  `https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${GITHUB_PR_NUMBER}/comments`,
+  {
+    headers: defaultHeaders,
+    method: "POST",
+    body: JSON.stringify({
+      body: GH_COMMENT,
+    }),
+  },
+)
+  .then((response) => {
+    if (response.ok) return response.json();
+    throw new Error(response.statusText);
+  })
+  .catch((err) => {
+    console.log("[COMMENT_ON_GITHUB: ERROR]");
+    throw new Error(err);
+  })
+  .finally(() => {
+    console.log("[COMMENT_ON_GITHUB: END]");
+  });
